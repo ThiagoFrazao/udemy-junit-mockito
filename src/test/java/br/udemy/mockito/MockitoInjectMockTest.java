ae90a423174f20de5cae5f43604cd9d7175197e8
Thiago da Costa Pinto Frazao de Sousa <f514189@corp.caixa.gov.br>
2025-01-13 21:39:18 -0300

    feat: exemplo de injection de mock

---
+++ src/test/java/br/udemy/mockito/MockitoInjectMockTest.java
@@ -0,0 +1,160 @@
+package br.udemy.mockito;
+
+import br.udemy.business.CourseBusiness;
+import br.udemy.exceptions.UnrecoverableException;
+import br.udemy.models.Course;
+import br.udemy.services.CourseService;
+import br.udemy.services.CourseServiceImpl;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.BDDMockito;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class MockitoInjectMockTest {
+
+    @InjectMocks
+    static CourseBusiness courseBusiness;
+
+    @Mock
+    static CourseService courseService;
+
+    @Captor
+    static ArgumentCaptor<Course> captorCourse;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Spring"})
+    void testCoursesRelatedToStringWhenUsingMockito(String tema) {
+        final String nomeAluno = RandomStringUtils.randomAlphabetic(10);
+        Mockito.when(courseBusiness.recuperarCursosPorAlunoETema(nomeAluno, tema)).thenReturn(gerarCursosAluno());
+        final List<Course> cursos = courseBusiness.recuperarCursosPorAlunoETema(nomeAluno, tema);
+        Assertions.assertFalse(cursos.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Oreilly"})
+    void testCoursesRelatedToOreillyWhenUsingMockito(String tema) {
+        final String nomeAluno = RandomStringUtils.random(10);
+        Mockito.when(courseBusiness.recuperarCursosPorAlunoETema(nomeAluno, tema)).thenReturn(gerarCursosAluno());
+        final List<Course> cursos = courseBusiness.recuperarCursosPorAlunoETema(nomeAluno, tema);
+        Assertions.assertTrue(cursos.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Oreilly"})
+    void testCoursesRelatedToOreillyTwoThenWhenUsingMockito(String tema) {
+        final String nomeAluno = RandomStringUtils.random(10);
+        Mockito.when(courseBusiness.recuperarCursosPorAlunoETema(nomeAluno, tema))
+                .thenReturn(gerarCursosAluno())
+                .thenReturn(gerarCursosAluno(tema, tema));
+        List<Course> cursos = courseBusiness.recuperarCursosPorAlunoETema(nomeAluno, tema);
+        Assertions.assertTrue(cursos.isEmpty());
+        cursos = courseBusiness.recuperarCursosPorAlunoETema(nomeAluno, tema);
+        Assertions.assertEquals(10, cursos.size());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"TesteTematico"})
+    void testCourseNameWithArgumentMatcher(String temaCurso) {
+        final String nomeCurso = RandomStringUtils.random(10);
+        List<Course> listaCursos = Mockito.mock(List.class);
+        Mockito.when(listaCursos.get(Mockito.anyInt()))
+                .thenReturn(new Course(nomeCurso, temaCurso));
+        final Course course = listaCursos.get(0);
+        Assertions.assertNotNull(course);
+        Assertions.assertEquals(nomeCurso, course.getNome());
+        Assertions.assertEquals(temaCurso, course.getTema());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Falha esperada do teste"})
+    void testCourseNameThrownsUnrecoverableException(String errorMsg) {
+        try {
+            final String nomeCurso = "FALHA_CURSO";
+            Mockito.when(courseBusiness.recuperarCursoPorNome(nomeCurso))
+                    .thenThrow(new UnrecoverableException(errorMsg));
+            courseBusiness.recuperarCursoPorNome(nomeCurso);
+            Assertions.fail("Deveria ter lancado uma exception %s".formatted(UnrecoverableException.class.getSimpleName()));
+        } catch (Exception e) {
+            Assertions.assertEquals(UnrecoverableException.class, e.getClass());
+            Assertions.assertEquals(errorMsg, e.getMessage());
+        }
+    }
+
+    /**
+     * Verify: testar metodos sem retorno (void) - verifica se ao chamar um metodo do "Business" o metodo correto do
+     * Service sera chamado com os parametros corretos!
+     * Mockito.times verifica quantas vezes o metodo esperado pelo Mockito.verify foi chamado
+     */
+    @Test
+    void testVerifyAddCourseWithoutReturn() {
+        Course course = new Course("CursoNovo","GERENCIADOR");
+        courseBusiness.adicionarNovoCurso(course);
+        Mockito.verify(courseService, Mockito.times(1)).adicionarCurso(course);
+    }
+
+    /**
+     * then: testar metodos sem retorno (void) - verifica se ao chamar um metodo do "Business" o metodo correto do
+     * Service sera chamado com os parametros corretos!
+     * Should - verifica se o metodo seguinte foi chamado
+     */
+    @Test
+    void testThenAddCourseWithoutReturn() {
+        Course course = new Course("CursoNovo","GERENCIADOR");
+        courseBusiness.adicionarNovoCurso(course);
+        BDDMockito.then(courseService).should().adicionarCurso(course);
+    }
+
+    ///then: testar metodos sem retorno (void) - verifica se ao chamar um metodo do "Business" o metodo correto do
+    ///Service sera chamado com os parametros corretos!
+    ///Should com Never - verifica se o metodo foi chamado, mas com um resultado diferente
+    @Test
+    void testThenNeverAddCourseDifferentName() {
+        Course course = new Course("CursoNovo","GERENCIADOR");
+        courseBusiness.adicionarNovoCurso(course);
+        BDDMockito.then(courseService).should(BDDMockito.never()).adicionarCurso(new Course("curso diferente"));
+    }
+
+    /**
+     * ArgumentCapture: captura os parametros enviados a um Mock para que possam ser validados
+     */
+    @Test
+    void testArgumentCapture() {
+        Course course = new Course("CursoNovo","GERENCIADOR");
+        courseBusiness.adicionarNovoCurso(course);
+        BDDMockito.then(courseService).should().adicionarCurso(captorCourse.capture());
+        Assertions.assertEquals(course, captorCourse.getValue());
+    }
+
+    private List<Course> gerarCursosAluno() {
+        return gerarCursosAluno("Udemy", "Spring");
+    }
+
+    private List<Course> gerarCursosAluno(String basePar, String baseImpar) {
+        List<Course> retorno = new ArrayList<>();
+        final String templateNome = "%s-%s";
+        for(int i = 0; i < 10; i++) {
+            if(i%2 == 0) {
+                retorno.add(new Course(templateNome.formatted(basePar , RandomStringUtils.random(10, basePar))));
+            } else {
+                retorno.add(new Course(templateNome.formatted(baseImpar, RandomStringUtils.random(10, baseImpar))));
+            }
+        }
+        return retorno;
+    }
+
+
+}
